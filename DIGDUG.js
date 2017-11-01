a0911ebf14121ea921fec044b1877e91c8cd5800
sindrigudmunds <sthg11@hi.is>
2017-11-01 16:35:06 +0000

    Upphafsgrindin
    Grind fyrir DigDug

---
+++ DIGDUG.js
@@ -0,0 +1,180 @@
+// ============================= //
+//  DIG DUG
+// ============================= //
+
+"use strict";
+
+var g_canvas = document.getElementById("myCanvas");
+var g_ctx = g_canvas.getContext("2d");
+
+
+
+
+// =====================
+// CREATE Player
+// =====================
+
+
+/*
+function createInitialShips() {
+
+    entityManager.generateShip({
+        cx : 200,
+        cy : 200
+    });
+
+}
+*/
+
+
+
+// =====================
+// GATHER INPUTS
+// =====================
+
+
+function gatherInputs(){
+  // Nothing to do here!
+  // The event handlers do everything we need for now.
+}
+
+
+// =================
+// UPDATE SIMULATION
+// =================
+
+// We take a very layered approach here...
+//
+// The primary `update` routine handles generic stuff such as
+// pausing, single-step, and time-handling.
+//
+// It then delegates the game-specific logic to `updateSimulation`
+
+
+// GAME-SPECIFIC UPDATE LOGIC
+
+function updateSimulation(du) {
+
+    processDiagnostics();
+
+    entityManager.update(du);
+
+    // Prevent perpetual firing!
+    //eatKey(Ship.prototype.KEY_FIRE);
+}
+
+
+// Eiður: Comment-a út: veit ekki hvort við notum sambærileg diagnostics
+// GAME-SPECIFIC DIAGNOSTICS
+/*
+var g_allowMixedActions = true;
+var g_useGravity = false;
+var g_useAveVel = true;
+var g_renderSpatialDebug = false;
+
+var KEY_MIXED   = keyCode('M');;
+var KEY_GRAVITY = keyCode('G');
+var KEY_AVE_VEL = keyCode('V');
+var KEY_SPATIAL = keyCode('X');
+
+var KEY_HALT  = keyCode('H');
+var KEY_RESET = keyCode('R');
+
+var KEY_0 = keyCode('0');
+
+var KEY_1 = keyCode('1');
+var KEY_2 = keyCode('2');
+
+var KEY_K = keyCode('K');
+
+function processDiagnostics() {
+
+    if (eatKey(KEY_MIXED))
+        g_allowMixedActions = !g_allowMixedActions;
+
+    if (eatKey(KEY_GRAVITY)) g_useGravity = !g_useGravity;
+
+    if (eatKey(KEY_AVE_VEL)) g_useAveVel = !g_useAveVel;
+
+    if (eatKey(KEY_SPATIAL)) g_renderSpatialDebug = !g_renderSpatialDebug;
+
+    if (eatKey(KEY_HALT)) entityManager.haltShips();
+
+    if (eatKey(KEY_RESET)) entityManager.resetShips();
+
+    if (eatKey(KEY_0)) entityManager.toggleRocks();
+
+    if (eatKey(KEY_1)) entityManager.generateShip({
+        cx : g_mouseX,
+        cy : g_mouseY,
+
+        sprite : g_sprites.ship});
+
+    if (eatKey(KEY_2)) entityManager.generateShip({
+        cx : g_mouseX,
+        cy : g_mouseY,
+
+        sprite : g_sprites.ship2
+        });
+
+    if (eatKey(KEY_K)) entityManager.killNearestShip(
+        g_mouseX, g_mouseY);
+}
+*/
+
+
+// =================
+// RENDER SIMULATION
+// =================
+
+// We take a very layered approach here...
+//
+// The primary `render` routine handles generic stuff such as
+// the diagnostic toggles (including screen-clearing).
+//
+// It then delegates the game-specific logic to `gameRender`
+
+
+// GAME-SPECIFIC RENDERING
+
+function renderSimulation(ctx) {
+
+    entityManager.render(ctx);
+
+    if (g_renderSpatialDebug) spatialManager.render(ctx);
+}
+
+
+// =============
+// PRELOAD STUFF
+// =============
+
+var g_images = {};
+
+function requestPreloads() {
+
+    var requiredImages = {
+        spriteSheet1   : "NES_Dig_Dug_General_Sprites.png",
+        spriteSheet2  : "NES_Dig_Dug_Vegetables.png",
+
+    };
+
+    imagesPreload(requiredImages, g_images, preloadDone);
+}
+
+var g_sprites = {};
+
+function preloadDone() {
+
+    g_sprites.spriteSheet1  = new Sprite(g_images.spriteSheet1);
+    g_sprites.spriteSheet2 = new Sprite(g_images.spriteSheet2);
+
+
+    entityManager.init();
+    createInitialShips();
+
+    main.init();
+}
+
+// Kick it off
+requestPreloads();
