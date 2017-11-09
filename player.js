fd13a98c65e595774a15e48ffdbefef30cd0a250
edsonedge <sprotabrot@googlemail.com>
2017-11-09 23:01:34 +0000

    cleanup breytingar

--- player.js
+++ player.js
@@ -15,7 +15,7 @@ Player.prototype = new Entity();
 //Player.prototype.cx = 480;
 //Player.prototype.cy = 480;
 
-Player.prototype.MIN_Y = 50;
+Player.prototype.MIN_Y = 30;
 Player.prototype.MAX_Y = 512;
 Player.prototype.MIN_X = 10; // ekki 0 til að sprite fari ekki út fyrir rammann
 Player.prototype.MAX_X = 438;// 448 - 10 (kall er 32 px á breidd)
